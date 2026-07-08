e12532680e9c4743b8086be9e5a920aa0dea370c
Amine Nahli.mvnw.cmd <nahli-ami@upf.ac.ma>
2026-07-08 19:22:43 +0100

    feat: add start-n8n powershell script to automate ngrok and n8n initialization

--- workflow/start-n8n.ps1
+++ workflow/start-n8n.ps1
@@ -1,10 +1,13 @@
 # Lance ngrok en arrière-plan
 Start-Process powershell -ArgumentList "-NoExit", "-Command", "ngrok http 5678 --domain=sycamore-splashy-prewar.ngrok-free.dev"
 
-# Attends 3 secondes que ngrok démarre
+# Attends 3 secondes que ngrok démarre  
+#06052005N
 Start-Sleep -Seconds 3
 
 # Lance n8n
-$env:WEBHOOK_URL = "https://sycamore-splashy-prewar.ngrok-free.dev/"
-$env:NODE_FUNCTION_ALLOW_BUILTIN = "child_process"
-npx n8n
+$env:WEBHOOK_URL="https://sycamore-splashy-prewar.ngrok-free.dev/"
+$env:NODE_FUNCTION_ALLOW_BUILTIN="child_process"
+$env:N8N_USER_MANAGEMENT_DISABLED="true"
+npx n8n 
+
